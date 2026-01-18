6fa0dcdc1a78641c0a7d41355b58cfb9a04587bc
agikwiyono <agikwiyon3@gmail.com>
2026-01-19 06:21:53 +0700

    1

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http
+import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String baseUrl = "http://192.168.100.192:8000/api";
